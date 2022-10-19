147c53a6fac9c9ec2ee8b27b0ee7daca09d0b5cd
ikemura-ren <utip.saikai.nozomu@icloud.com>
2022-10-19 20:13:58 +0900

    ルーティング用のディレクトリ作成

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import(
-	"net/http"
+	"todo/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,8 +9,6 @@ func main() {
     engine:= gin.Default()
 	engine.LoadHTMLGlob("views/*.html")
 	engine.Static("/static", "./static")
-    engine.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", gin.H{})
-        })
+    engine.GET("/", routes.Home) 
     engine.Run(":8080")
 }
