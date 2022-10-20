c971aa80af8f515f6394fb27421a81ca9d58db1d
ikemura-ren <utip.saikai.nozomu@icloud.com>
2022-10-20 21:11:10 +0900

    Connection.goにコメント作成

--- database/Connection.go
+++ database/Connection.go
@@ -1,23 +1,23 @@
 package database
 
 import(
-	"database/sql"
+	"database/sql"//データベースとの接続のためのインターフェース
 	"log"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql" // MySQLのコネクションドライバ
 )
 
 func ConnectionDB(){
-	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/go_todo")
+	db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/go_todo") // ローカルのMySQLのDSN
     if err!= nil {
-        log.Println(err.Error())
+        log.Println(err.Error())//もし変数errがnilではない場合、エラーログを表示
     }
-	defer db.Close()
+	defer db.Close()//遅延実行で最終的にはコネクションを閉じている
 
-	err = db.Ping()
+	err = db.Ping()//この関数はDBとの接続を監視しており、必要であれば接続を管理する
     if err!= nil {
 		log.Fatal(err)
 	} else {
-		log.Println("Connected to the database")
+		log.Println("Connected to the database")//接続が確立していたら出る内容
 	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import(
 	"todo/routes"
-	"todo/database"
+	//"todo/database"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,7 +10,7 @@ func main() {
     engine:= gin.Default()
 	engine.LoadHTMLGlob("views/*.html")
 	engine.Static("/static", "./static")
-	database.ConnectionDB()
+	//database.ConnectionDB()
     engine.GET("/", routes.Home) 
     engine.Run(":8080")
 }
